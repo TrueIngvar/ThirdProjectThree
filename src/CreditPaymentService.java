97aff63e79ebd375203361a978813d9de2012e57
TrueIngvar <igor171298@mail.ru>
2024-01-15 10:05:19 +0400

    Reformatted code and removed comments

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,18 +2,18 @@ public class CreditPaymentService {
     public double calculate(int creditAmount, int creditTerm) {
         if (creditTerm == 1) {
             creditTerm = 12;
-        };
+        }
         if (creditTerm == 2) {
             creditTerm = 24;
-        };
+        }
         if (creditTerm == 3) {
             creditTerm = 36;
-        };
+        }
         double interestRate = 9.99;
         double monthlyInterestRate = interestRate / (100 * 12);
         double numerator = monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, creditTerm));
         double denominator = (Math.pow(1 + monthlyInterestRate, creditTerm)) - 1;
-        double monthlyPayment = creditAmount * (numerator/denominator);
+        double monthlyPayment = creditAmount * (numerator / denominator);
         return monthlyPayment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double monthlyPayment = service.calculate(1_000_000, 3);
+        double monthlyPayment = service.calculate(1_000_000, 1);
         System.out.println((int) monthlyPayment);
-        }
     }
+}
