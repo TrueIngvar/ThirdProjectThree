21cd1f1f0d1b18405a01f56e071f8bf7ef18a20f
TrueIngvar <igor171298@mail.ru>
2024-01-15 11:29:00 +0400

    Removed conditions and added type int

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,18 +1,10 @@
 public class CreditPaymentService {
     public double calculate(int creditAmount, int creditTerm) {
-        if (creditTerm == 1) {
-            creditTerm = 12;
-        }
-        if (creditTerm == 2) {
-            creditTerm = 24;
-        }
-        if (creditTerm == 3) {
-            creditTerm = 36;
-        }
+        int multipliedCreditTerm = creditTerm * 12;
         double interestRate = 9.99;
         double monthlyInterestRate = interestRate / (100 * 12);
-        double numerator = monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, creditTerm));
-        double denominator = (Math.pow(1 + monthlyInterestRate, creditTerm)) - 1;
+        double numerator = monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, multipliedCreditTerm));
+        double denominator = (Math.pow(1 + monthlyInterestRate, multipliedCreditTerm)) - 1;
         double monthlyPayment = creditAmount * (numerator / denominator);
         return monthlyPayment;
     }
